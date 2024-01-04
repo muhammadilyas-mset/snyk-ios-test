b36b7ff0b52576ec628e1fceca8a9c96ef29dcd6
muhammadilyas-aym <muhammadi@aymcommerce.com>
2024-01-04 18:19:27 +0500

    initial pods commit

---
+++ snyk_ios_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snyk_ios_test
+//
+//  Created by Mac Mini 2014 Late on 04/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
